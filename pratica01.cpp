89b287156a7d9d7d9d462ca48cbc91966f868257
Alberto1609 <60992018+Alberto1609@users.noreply.github.com>
2020-02-13 00:10:51 -0600

    Add files via upload

---
+++ pratica01.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+float a,b,c;
+int opt;
+printf("1. Suma \n2. Resta \n3. Multiplicacion \n4. Divicion");
+printf("\n\nSeleccione una opcion: ");
+scanf("%d",&opt);
+
+printf("\nIntroduce el primer valor: ");
+scanf("%f",&a);
+printf("\nIntroduce el segundo valor: ");
+scanf("%f",&b);
+
+if(opt==1)
+ {
+    printf("\nLa suma es:%f ",c=a+b);
+ }
+  else if(opt==2)
+  {
+    printf("\nLa resta es:%f ",c=a-b);
+  }
+   else if(opt==3)
+   {
+    printf("\nLa multiplicacion es:%f ",c=a*b);
+   }
+    else if(opt==4)
+    {
+    while(b==0)
+    {
+        printf("\nError, b debe de ser diferente de 0");
+        printf("\nIntrodusca el segundo operando");
+        scanf("%f",&b);
+    }
+    printf("\nLa divivion es:%f ",c=a/b);
+    }
+     else
+     {
+     printf("\nValor incorrecto");
+     }
+}
